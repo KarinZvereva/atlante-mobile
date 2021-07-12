a2715c7212c46df6997253ea4086771a26f8c9d3
Daniele Accattoli <d.accattoli@teamsystem.com>
2021-07-12 17:51:48 +0200

    added search on touch

--- src/screens/WineriesMap/MarkerPopup.tsx
+++ src/screens/WineriesMap/MarkerPopup.tsx
@@ -1,49 +1,168 @@
 import React, {useState, useEffect} from 'react';
 import {Winery} from '../../common/interfaces';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, StyleSheet, Alert} from 'react-native';
+import {wineryLogoDal} from './WineriesMap.dal';
 
-export const MarkerPopup = React.memo(({winery}: {winery: Winery}) => {
-  const [logo, setLogo] = useState<string>();
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    alignSelf: 'flex-start',
+  },
+  bubble: {
+    width: 140,
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+    backgroundColor: '#4da2ab',
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+    borderRadius: 6,
+    borderColor: '#007a87',
+    borderWidth: 0.5,
+  },
+  amount: {
+    flex: 1,
+  },
+  arrow: {
+    backgroundColor: 'transparent',
+    borderWidth: 16,
+    borderColor: 'transparent',
+    borderTopColor: '#4da2ab',
+    alignSelf: 'center',
+    marginTop: -32,
+  },
+  arrowBorder: {
+    backgroundColor: 'transparent',
+    borderWidth: 16,
+    borderColor: 'transparent',
+    borderTopColor: '#007a87',
+    alignSelf: 'center',
+    marginTop: -0.5,
+  },
+});
 
-  useEffect(() => {}, []);
+interface IMarkerPopupProps {
+  winery: Winery;
+  style?: any;
+  children?: any;
+}
+
+export const MarkerPopup = React.memo(({winery}: IMarkerPopupProps) => {
+  const [logo, setLogo] = useState<string | null>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    wineryLogoDal
+      .getById(winery._id)
+      .then((result) => {
+        if (result && result.data) {
+          setLogo(result.data.logo);
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(JSON.stringify(error));
+        Alert.alert('Logo non caricato');
+        setLoading(false);
+      });
+  }, []);
 
   return (
-    <View style={{flex: 1, flexDirection: 'column'}}>
-      <View style={{flexDirection: 'row'}}>
-        <View>
-          {!logo && <Text>NO LOGO</Text>}
-          {logo && (
-            <Image
-              source={{uri: logo}}
-              style={{width: 120, height: 120, resizeMode: 'center'}}
-            />
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          {!loading && !logo && <Text>NO LOGO</Text>}
+          {!loading && logo && (
+            <Text style={{height: 140}}>
+              <Image
+                source={{uri: logo}}
+                style={{width: 120, height: 120, resizeMode: 'contain'}}
+              />
+            </Text>
           )}
         </View>
-        <View style={{flexDirection: 'column'}}>
-          <View style={{flexDirection: 'row'}}>
-            <Image
-              source={require('../../assets/icon/tel_popup.png')}
-              style={{width: 10, height: 10, margin: 20, resizeMode: 'center'}}
-            />
-            <Image
-              source={require('../../assets/icon/mail_popup.png')}
-              style={{width: 10, height: 10, margin: 20, resizeMode: 'center'}}
-            />
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text style={{height: 60}}>
+              <Image
+                source={require('../../assets/icon/tel_popup.png')}
+                style={{
+                  width: 40,
+                  height: 40,
+                  margin: 20,
+                  resizeMode: 'contain',
+                }}
+              />
+            </Text>
+            <Text style={{height: 60}}>
+              <Image
+                source={require('../../assets/icon/mail_popup.png')}
+                style={{
+                  width: 40,
+                  height: 40,
+                  margin: 20,
+                  resizeMode: 'contain',
+                }}
+              />
+            </Text>
           </View>
-          <View style={{flexDirection: 'row'}}>
-            <Image
-              source={require('../../assets/icon/web_popup.png')}
-              style={{width: 10, height: 10, margin: 20, resizeMode: 'center'}}
-            />
-            <Image
-              source={require('../../assets/icon/go_to_popup.png')}
-              style={{width: 10, height: 10, margin: 20, resizeMode: 'center'}}
-            />
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text style={{height: 60}}>
+              <Image
+                source={require('../../assets/icon/web_popup.png')}
+                style={{
+                  width: 40,
+                  height: 40,
+                  margin: 20,
+                  resizeMode: 'contain',
+                }}
+              />
+            </Text>
+            <Text style={{height: 60}}>
+              <Image
+                source={require('../../assets/icon/go_to_popup.png')}
+                style={{
+                  width: 40,
+                  height: 40,
+                  margin: 20,
+                  resizeMode: 'contain',
+                }}
+              />
+            </Text>
           </View>
         </View>
       </View>
       <Text>{winery.name}</Text>
-      <Text>{winery.vigneron}</Text>
+      {winery.vigneron && <Text>{winery.vigneron}</Text>}
       <Text>{winery.address}</Text>
       <Text>{winery.city}</Text>
       <Text>{winery.province}</Text>
--- src/screens/WineriesMap/WineriesMap.tsx
+++ src/screens/WineriesMap/WineriesMap.tsx
@@ -9,7 +9,7 @@ import {
   Dimensions,
   Alert,
 } from 'react-native';
-import MapView, {Region} from 'react-native-maps';
+import MapView, {LatLng, Region} from 'react-native-maps';
 import {Header} from '../../common/components/Header/Header';
 import Geolocation from '@react-native-community/geolocation';
 import {COORDINATES_DELTA} from '../../common/constants/coordinates';
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     position: 'absolute',
-    top: 70,
+    bottom: 20,
     right: 20,
     borderRadius: 30,
     backgroundColor: '#d2d2d2',
@@ -71,8 +71,19 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     position: 'absolute',
-    bottom: 20,
+    top: 70,
     right: 20,
+    borderRadius: 30,
+    backgroundColor: '#d2d2d2',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cleanButton: {
+    width: 40,
+    height: 40,
+    position: 'absolute',
+    top: 70,
+    right: 65,
     borderRadius: 30,
     backgroundColor: '#d2d2d2',
     alignItems: 'center',
@@ -112,35 +123,79 @@ export const WineriesMap = (props: any) => {
   const [loading, setLoading] = useState<boolean>(false);
   const [data, setData] = useState<Winery[]>();
   const [search, setSearch] = useState<string>('');
+  const [selectedPosition, setSelectedPosition] = useState<LatLng>();
+  const [markerRefs, setMarkerRefs] = useState<React.RefObject<Marker>[]>([]);
   const map = useRef<MapView>(null);
 
-  const loadWineriesCallback = useCallback((filters?: string) => {
-    setLoading(true);
-    if (data) setData(undefined);
-    wineryDataDal
-      .get({
-        filter: filters
-          ? `${wineryFilterBase} AND ${filters}`
-          : wineryFilterBase,
-      })
-      .then((result) => {
-        if (result && result.data) {
-          setData(result.data.data || []);
+  const loadWineriesCallback = useCallback(
+    (filters?: string) => {
+      setLoading(true);
+      if (data) setData(undefined);
+      wineryDataDal
+        .get({
+          filter: filters
+            ? `${wineryFilterBase} AND ${filters}`
+            : wineryFilterBase,
+          pageNumber: 1,
+          pageSize: 30,
+        })
+        .then((result) => {
+          if (result && result.data) {
+            setData(result.data.data || []);
+            setLoading(false);
+          }
+        })
+        .catch((e) => {
+          console.error(JSON.stringify(e));
+          Alert.alert(
+            `Connessione con il server non riuscita, uscire e riprova in seguito con una miglior connessione!`,
+          );
           setLoading(false);
-        }
-      })
-      .catch((e) => {
-        console.error(JSON.stringify(e));
-        Alert.alert(
-          `Connessione con il server non riuscita, uscire e riprova in seguito con una miglior connessione!`,
-        );
-        setLoading(false);
-      });
-  }, []);
+        });
+    },
+    [data],
+  );
 
-  useEffect(() => {
-    loadWineriesCallback();
-  }, []);
+  const loadWineriesFromCoordinates = useCallback(
+    ({latitude, longitude}: LatLng) => {
+      setLoading(true);
+      if (data) setData(undefined);
+      wineryDataDal
+        .around({
+          lat: latitude,
+          lon: longitude,
+          radius: 40,
+        })
+        .then((result) => {
+          if (result && result.data) {
+            const res = result.data.filter(
+              (w) =>
+                w.type === WineryType.Winery ||
+                w.type === WineryType.Vigneron_Itinerant ||
+                w.type === WineryType.Wine_Project ||
+                w.type === WineryType.Wrong_Position,
+            );
+            setData(res);
+            setLoading(false);
+            if (map && map.current)
+              map.current.fitToCoordinates(
+                res.map((r) => ({
+                  latitude: r.location?.latitude!,
+                  longitude: r.location?.longitude!,
+                })),
+                {animated: true},
+              );
+          }
+        })
+        .catch(() => {
+          Alert.alert(
+            `Connessione con il server non riuscita, riprova in seguito!`,
+          );
+          setLoading(false);
+        });
+    },
+    [data, map],
+  );
 
   const gotToMyLocation = useCallback(() => {
     Geolocation.getCurrentPosition(
@@ -168,42 +223,10 @@ export const WineriesMap = (props: any) => {
   const filterWithMyPosition = useCallback(() => {
     Geolocation.getCurrentPosition(
       ({coords}) => {
-        setLoading(true);
-        if (data) setData(undefined);
-        wineryDataDal
-          .around({
-            lat: coords.latitude,
-            lon: coords.longitude,
-            radius: 40,
-          })
-          .then((result) => {
-            if (result && result.data) {
-              const res = result.data.filter(
-                (w) =>
-                  w.type === WineryType.Winery ||
-                  w.type === WineryType.Vigneron_Itinerant ||
-                  w.type === WineryType.Wine_Project ||
-                  w.type === WineryType.Wrong_Position,
-              );
-              setData(res);
-              setLoading(false);
-              if (map && map.current)
-                map.current.fitToCoordinates(
-                  res.map((r) => ({
-                    latitude: r.location?.latitude!,
-                    longitude: r.location?.longitude!,
-                  })),
-                  {animated: true},
-                );
-            }
-          })
-          .catch((e) => {
-            console.error(JSON.stringify(e));
-            Alert.alert(
-              `Connessione con il server non riuscita, uscire e riprova in seguito con una miglior connessione!`,
-            );
-            setLoading(false);
-          });
+        loadWineriesFromCoordinates({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        });
       },
       (error) => {
         console.error(JSON.stringify(error));
@@ -211,6 +234,27 @@ export const WineriesMap = (props: any) => {
       },
       {enableHighAccuracy: true},
     );
+  }, [loadWineriesFromCoordinates]);
+
+  useEffect(() => {
+    loadWineriesCallback();
+  }, []);
+
+  useEffect(() => {
+    if (selectedPosition) loadWineriesFromCoordinates(selectedPosition);
+  }, [selectedPosition]);
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setMarkerRefs((markerRefs) => {
+        return Array(data.length)
+          .fill(1)
+          .map(
+            (_, i) =>
+              markerRefs[i] || React.createRef<React.RefObject<Marker>>(),
+          );
+      });
+    } else setMarkerRefs([]);
   }, [data]);
 
   return (
@@ -219,21 +263,42 @@ export const WineriesMap = (props: any) => {
         initialRegion={InitialRegion}
         style={styles.map}
         ref={map}
-        showsUserLocation={true}>
-        {data &&
-          data.map((d) => (
+        showsUserLocation={true}
+        onLongPress={(event) => {
+          setSelectedPosition(event.nativeEvent.coordinate);
+        }}>
+        <>
+          {data &&
+            data.map((d, i) => (
+              <Marker
+                key={d._id}
+                coordinate={{
+                  latitude: d.location?.latitude || 0,
+                  longitude: d.location?.longitude || 0,
+                }}
+                icon={require('../../assets/icon/winery_marker.png')}
+                ref={markerRefs[i]}
+                onPress={() => {
+                  setTimeout(() => {
+                    markerRefs[i].current?.hideCallout();
+                    markerRefs[i].current?.showCallout();
+                  }, 400);
+                }}>
+                <Callout>
+                  <MarkerPopup winery={d} />
+                </Callout>
+              </Marker>
+            ))}
+          {selectedPosition && (
             <Marker
-              key={d._id}
+              key={'Selected_position'}
               coordinate={{
-                latitude: d.location?.latitude || 0,
-                longitude: d.location?.longitude || 0,
+                latitude: selectedPosition.latitude,
+                longitude: selectedPosition.longitude,
               }}
-              icon={require('../../assets/icon/winery_marker.png')}>
-              <Callout>
-                <MarkerPopup winery={d} />
-              </Callout>
-            </Marker>
-          ))}
+            />
+          )}
+        </>
       </MapView>
       <Header {...props} showName="Wineries Map" />
       {loading && (
@@ -241,14 +306,6 @@ export const WineriesMap = (props: any) => {
           <ActivityIndicator size="large" color={markerDefaultGreen} />
         </View>
       )}
-      <TouchableOpacity
-        onPress={() => gotToMyLocation()}
-        style={styles.goToMyPositionButton}>
-        <Image
-          style={{width: 40, height: 40}}
-          source={require('../../assets/icon/posizione.png')}
-        />
-      </TouchableOpacity>
       <TouchableOpacity
         style={styles.reloadButton}
         onPress={() => {
@@ -258,6 +315,26 @@ export const WineriesMap = (props: any) => {
         <Image
           style={{width: 40, height: 40}}
           source={require('../../assets/icon/reload.png')}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.cleanButton}
+        onPress={() => {
+          setData(undefined);
+          setSelectedPosition(undefined);
+        }}
+        disabled={loading}>
+        <Image
+          style={{width: 40, height: 40}}
+          source={require('../../assets/icon/clean_map.png')}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => gotToMyLocation()}
+        style={styles.goToMyPositionButton}>
+        <Image
+          style={{width: 40, height: 40}}
+          source={require('../../assets/icon/posizione.png')}
         />
       </TouchableOpacity>
       <TouchableOpacity
