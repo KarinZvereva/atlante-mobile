c98d0948fc9f1139914c7b88b574e1ceca474bdf
Diego <diegomateddu@gmail.com>
2021-09-07 19:03:04 +0200

    #5 New layout

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,22 +1,23 @@
 import React, {useContext, useState, useRef} from 'react';
-import {View, TextInput, Text, Image, ScrollView, Alert} from 'react-native';
+import {View, TextInput, Text, Image, Alert} from 'react-native';
 import {Header} from '../../common/components/Header/Header';
 import {icons} from '../../common/constants';
 import {AuthContext, ITokenData, AuthTokenManager} from '../../common/modules/auth';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import LinearGradient from 'react-native-linear-gradient';
 import {styles} from './Profile.styles';
-import { SafeAreaView } from 'react-native';
+import {SafeAreaView} from 'react-native';
 import Recaptcha, {RecaptchaHandles} from 'react-native-recaptcha-that-works';
 import {User} from '../../common/interfaces/web-api';
 import {ProfileDal} from './Profile.dal';
 import {webCaptchaUrl, captchaSiteKey} from '../../common/constants';
-import { UserAuthenticationMode } from '../../common/modules/auth/auth.constants';
+import {UserAuthenticationMode} from '../../common/modules/auth/auth.constants';
+
 
 export function Profile(props: any) {
   const actionsProvider = useContext(AuthContext);
   const userInfo = AuthTokenManager.decodeToken(actionsProvider.data.userToken) as ITokenData
-  const isFacebookAutenticated = userInfo != null ? userInfo.authentication == UserAuthenticationMode.FACEBOOK : "";
+  const isFacebookAutenticated = userInfo != null ? userInfo.authentication == UserAuthenticationMode.FACEBOOK : false;
   const [password, setPassword] = useState<string>();
   const [passwordConfirm, setPasswordConfirm] = useState<string>();
   const [email, setEMail] = useState<string>(userInfo != null ? userInfo.email : "");
@@ -48,7 +49,6 @@ export function Profile(props: any) {
 
     recaptcha.current?.open();
   };
-
 
   /**
    *
@@ -123,113 +123,118 @@ export function Profile(props: any) {
   return (
     <SafeAreaView style={styles.container}>
       <Header {...props} showName="Profilo Utente" />
-        <Image source={icons.profilo} style={styles.logo} />        
-        <View style={styles.profile_container}>
-          <Text style={styles.title_text}>{firstName} {lastName}</Text>
-          <Text style={styles.normal_text}>{email}</Text>
-        </View>
-        { (!showEdit && !isFacebookAutenticated) &&
-          <View style={styles.modify_profile_container}>
-            <LinearGradient
-              colors={['#ce8a86', '#bd6665', '#a92a3f']}
-              style={styles.modifyBtn}>
-              <TouchableOpacity
-                onPress={() => setShowEdit(true)}
-                disabled={!actionsProvider}>
-                <View style={styles.modifyBtnSubView}>
-                  <Text style={styles.buttonText}>Modifica</Text>
-                </View>
-              </TouchableOpacity>
-            </LinearGradient>
-          </View>
-        }
-        { showEdit && 
-        <ScrollView style={styles.scroll_container}>
-          <View style={styles.modify_profile_container}>
-            <View style={styles.inputView}>
-              <TextInput
-                style={styles.TextInput}
-                placeholder="Nome"
-                placeholderTextColor="#ffffff"
-                onChangeText={(value) => setFirstname(value)}
-                value= {firstName}
-              />
+      <View style={styles.profile_container}>
+        <Image source={icons.profilo_big} style={styles.logo} />
+        <Text style={styles.title_text}>{firstName}</Text>
+        <Text style={styles.title_text}>{lastName}</Text>
+        <Text style={styles.mail_text}>{email}</Text>
+      </View>
+      { (!showEdit && !isFacebookAutenticated) &&
+      <View style={styles.modify_profile_container}>
+        <LinearGradient
+          colors={['#ce8a86', '#bd6665', '#a92a3f']}
+          style={styles.modifyBtn}>
+          <TouchableOpacity
+            onPress={() => setShowEdit(true)}
+            disabled={!actionsProvider}>
+            <View style={styles.modifyBtnSubView}>
+              <Text style={styles.buttonText}>Modifica</Text>
             </View>
-            <View style={styles.inputView}>
-              <TextInput
-                style={styles.TextInput}
-                placeholder="Cognome"
-                placeholderTextColor="#ffffff"
-                onChangeText={(value) => setLastname(value)}
-                value= {lastName}
-              />
-            </View>
-            <View style={styles.inputView}>
-              <TextInput
-                style={styles.TextInput}
-                placeholder="Password"
-                placeholderTextColor="#ffffff"
-                secureTextEntry={true}
-                onChangeText={(value) => setPassword(value)}
-              />
-            </View>
-            <View style={styles.inputView}>
-              <TextInput
-                style={styles.TextInput}
-                placeholder="Conferma Password"
-                placeholderTextColor="#ffffff"
-                secureTextEntry={true}
-                onChangeText={(value) => setPasswordConfirm(value)}
-              />
-            </View>
-            <View style={styles.button_container}>
-              <LinearGradient
-                colors={['#ce8a86', '#bd6665', '#a92a3f']}
-                style={styles.saveBtn}>
-                <TouchableOpacity
-                  onPress={() => Modify()}
-                  disabled={!actionsProvider}>
-                  <View style={styles.modifyBtnSubView}>
-                    <Text style={styles.buttonText}>Aggiorna</Text>
-                  </View>
-                </TouchableOpacity>
-              </LinearGradient>
-              <LinearGradient
-                colors={['#423E3F', '#605D5E', '#7F7C7D']}
-                style={styles.undoBtn}>
-                <TouchableOpacity
-                  onPress={() => {
-                      setShowEdit(false);
-                      setError("");
-                      setIsError(false);
-                    }}
-                  disabled={!actionsProvider}>
-                  <View style={styles.modifyBtnSubView}>
-                    <Text style={styles.buttonText}>Annulla</Text>
-                  </View>
-                </TouchableOpacity>
-              </LinearGradient>
-            </View>
-          </View>
-          </ScrollView>
-        }
-        {isError && (
-          <View style={{alignItems:'center'}}>
-            <Text style={{paddingTop: 5, color: 'red'}}>{error}</Text>
-          </View>
-        )}        
-        <View>
-          <Recaptcha
-            ref={recaptcha}
-            siteKey={captchaSiteKey}
-            baseUrl={webCaptchaUrl}
-            onVerify={onVerify}
-            onExpire={onExpire}
-            onError={onError}
-            size="invisible"
-            style={styles.recaptcha_container}
+          </TouchableOpacity>
+        </LinearGradient>
+      </View>
+      }
+      {(isFacebookAutenticated) &&
+      <View style={styles.modify_profile_container}>
+        <Text style={styles.facebook_text}>Non è possibile modificare il profilo</Text>
+        <Text style={styles.facebook_text}>Le informazioi sopra riportate provengono da Facebook</Text>
+      </View>
+      }   
+      { showEdit && 
+      <View style={styles.modify_profile_container}>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Nome"
+            placeholderTextColor="#ffffff"
+            onChangeText={(value) => setFirstname(value)}
+            value= {firstName}
           />
         </View>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Cognome"
+            placeholderTextColor="#ffffff"
+            onChangeText={(value) => setLastname(value)}
+            value= {lastName}
+          />
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Password"
+            placeholderTextColor="#ffffff"
+            secureTextEntry={true}
+            onChangeText={(value) => setPassword(value)}
+          />
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Conferma Password"
+            placeholderTextColor="#ffffff"
+            secureTextEntry={true}
+            onChangeText={(value) => setPasswordConfirm(value)}
+          />
+        </View>        
+        <View style={styles.button_container}>
+          <LinearGradient
+            colors={['#ce8a86', '#bd6665', '#a92a3f']}
+            style={styles.saveBtn}>
+            <TouchableOpacity
+              onPress={() => Modify()}
+              disabled={!actionsProvider}>
+              <View style={styles.modifyBtnSubView}>
+                <Text style={styles.buttonText}>Aggiorna</Text>
+              </View>
+            </TouchableOpacity>
+          </LinearGradient>
+          <LinearGradient
+            colors={['#423E3F', '#605D5E', '#7F7C7D']}
+            style={styles.undoBtn}>
+            <TouchableOpacity
+              onPress={() => {
+                  setShowEdit(false);
+                  setError("");
+                  setIsError(false);
+                }}
+              disabled={!actionsProvider}>
+              <View style={styles.modifyBtnSubView}>
+                <Text style={styles.buttonText}>Annulla</Text>
+              </View>
+            </TouchableOpacity>
+          </LinearGradient>
+        </View>            
+      </View>
+      }
+      {isError && (
+        <View style={{alignItems:'center'}}>
+          <Text style={{paddingTop: 5, color: 'red'}}>{error}</Text>
+        </View>
+      )}  
+      <View>
+        <Recaptcha
+          ref={recaptcha}
+          siteKey={captchaSiteKey}
+          baseUrl={webCaptchaUrl}
+          onVerify={onVerify}
+          onExpire={onExpire}
+          onError={onError}
+          size="invisible"
+          style={styles.recaptcha_container}
+        />
+      </View>
     </SafeAreaView>
   );
 }
