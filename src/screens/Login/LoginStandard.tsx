1fb4f9d5bc81cf0baf35c53edf80aefe2f9c5e06
Diego <diegomateddu@gmail.com>
2021-09-14 15:32:46 +0200

    log removed

--- src/screens/Login/LoginStandard.tsx
+++ src/screens/Login/LoginStandard.tsx
@@ -34,6 +34,7 @@ export function LoginStandard(props: any) {
     setLoading(true);
     AuthDal.login({userName, password})
       .then((res) => {
+        console.log(res)
         if (!res.token || !res.refreshToken) {
           setError('Dati errati');
           setIsError(true);
