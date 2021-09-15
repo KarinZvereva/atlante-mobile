d2508f9b172761cb1f30599db6657b825d3e9c75
Daniele Accattoli <d.accattoli@teamsystem.com>
2021-09-15 15:39:47 +0200

    #35 corrette diciture per la Map Info

--- src/screens/MapInfo/MapInfo.tsx
+++ src/screens/MapInfo/MapInfo.tsx
@@ -23,8 +23,8 @@ export const MapInfo = React.memo(() => {
             <RoundImageButton borderRadius={20} image={icons.touch} />
             <View style={mapsInfoStyles.divider}></View>
             <Text style={mapsInfoStyles.info_row_text}>
-              Tieni premuto su un punto della mappa e visualizza le cantine nel
-              raggio di 40 km
+              Tieni premuto su un punto della mappa e visualizza solo le cantine
+              vicine
             </Text>
           </View>
           <View style={mapsInfoStyles.info_row}>
@@ -38,7 +38,7 @@ export const MapInfo = React.memo(() => {
             <Image source={icons.intorno} style={mapsInfoStyles.info_image} />
             <View style={mapsInfoStyles.divider} />
             <Text style={mapsInfoStyles.info_row_text}>
-              Visualizza solo le cantine situate nel raggio di 40 km dalla tua
+              Visualizza solo le cantine situate nell'intorno della tua
               posizione
             </Text>
           </View>
@@ -68,8 +68,8 @@ export const MapInfo = React.memo(() => {
             </Text>
           </View>
           <Text style={mapsInfoStyles.search_text}>
-            Con GPS attivo la mappa visualizza inizialmente solo le cantine
-            nell'arco di 120 km dalla posizione attuale.
+            Con GPS attivo, all'apertura, vengono visualizzate solo le cantine
+            nel raggio di 120 km dalla posizione rilevata.
           </Text>
           <Text style={mapsInfoStyles.search_text}>
             Per visualizzare la mappa completa è sufficente premere sul pulsante
